09e997803791396566429d2305cf4f65a281fd9e
valosekj <jan.valosek@upol.cz>
2021-09-23 15:56:10 +0200

    fix script path for bash and zsh - #3

--- fsleyes_preset.sh
+++ fsleyes_preset.sh
@@ -16,13 +16,12 @@ if [[ $# -eq 0 ]] || [[ $1 =~ "-h" ]];then
     echo -e "\nEXAMPLE:\n\t${0##*/} sub-001_T1w_seg.nii.gz sub-001_T1w_gmseg.nii.gz ..."
 else
     # Get path to the python script
-    if [[ ${SHELL} == "/bin/bash" ]];then
-    	script_path=$(dirname $(realpath -s "$0"))
-    elif [[ ${SHELL} == "/bin/zsh" ]];then
+#    if [[ ${SHELL} == "/bin/bash" ]];then
+#    	script_path=$(dirname $(realpath -s "$0"))
+#    elif [[ ${SHELL} == "/bin/zsh" ]];then
 	script_path=$(dirname $(readlink "$0"))
-    fi
+#    fi
 
     # Call python script and pass all input arguments
     "${script_path}"/venv/bin/python "${script_path}"/fsleyes_preset.py "$@"
 fi
-
